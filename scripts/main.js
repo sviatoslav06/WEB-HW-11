ebadb1689fa318ec0c41be405f77b3e3db190bf6
Святослав Самолюк <samoliuk.sviatoslav@gmail.com>
2023-09-04 23:39:41 +0300

    Created Massive

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,10 @@
 const createBtn = document.querySelector('#create-btn');
 const clearBtn = document.querySelector('#clear-btn');
+const findBtn = document.querySelector('#find-btn');
 const model = document.querySelector('#model');
 const mark = document.querySelector('#mark');
 const speed = document.querySelector('#speed');
 const tableBody = document.querySelector('#carTable tbody');
-// const findBtn = document.querySelector('#find-btn');
-// const name = document.querySelector('.name');
-// const find = document.querySelector('#find');
-
-// findBtn.onclick = (e) => {
-//     if(find.value.toLowerCase() == name.value.toLowerCase()){
-
-//     }
-// }
-
-createBtn.onclick = (e) => {
-    if (model.value != '' && mark.value != '' && speed.value != '') {
-        let car = new Car(mark.value, model.value, speed.value);
-        console.log(car);
-        addCarToTable(car);
-    }
-    else alert('Enter all inputs!!!');
-}
-
-clearBtn.onclick = () => {
-    tableBody.innerHTML = '';
-}
-
-function addCarToTable(car) {
-    tableBody.innerHTML += `<tr>
-                                <td class="name">${car.mark}</td>
-                                <td>${car.model}</td>
-                                <td>${car.speed} км/год</td>
-                            </tr>`;
-}
 
 class Car {
     constructor(mark, model, speed) {
@@ -41,4 +12,59 @@ class Car {
         this.model = model;
         this.speed = speed;
     }
+}
+
+class Cars {
+    constructor() {
+        this.cars = new Array();
+    }
+    AddCar(mark, model, speed) {
+        // let car = new Car(mark, model, speed);   
+        this.cars += new Car(mark, model, speed);
+    }
+    Search(name) {
+        for (let i = 0; i < this.cars.length; i++) {
+            if (cars[i].mark.toLowerCase() == name.toLowerCase()) {
+                tableBody.innerHTML += `<tr>
+                                <td class="name">${cars[i].mark}</td>
+                                <td>${cars[i].model}</td>
+                                <td>${cars[i].speed} км/год</td>
+                            </tr>`;
+            }
+        }
+    }
+    DrowToTable() {
+        tableBody.innerHTML = '';
+        for (let i = 0; i < this.cars.length; i++) {
+            tableBody.innerHTML += `<tr>
+                                <td class="name">${cars[i].mark}</td>
+                                <td>${cars[i].model}</td>
+                                <td>${cars[i].speed} км/год</td>
+                            </tr>`;
+        }
+    }
+    Clear() {
+        tableBody.innerHTML = '';
+        this.cars = null;
+    }
+}
+
+let cars = new Cars();
+
+findBtn.onclick = (e) => {
+    Search();
+}
+
+createBtn.onclick = (e) => {
+    if (model.value != '' && mark.value != '' && speed.value != '') {
+        // let car = new Car(mark.value, model.value, speed.value);
+        cars.AddCar(mark.value, model.value, speed.value);
+        console.log(cars);
+        cars.DrowToTable();
+    }
+    else alert('Enter all inputs!!!');
+}
+
+clearBtn.onclick = () => {
+    cars.Clear();
 }
